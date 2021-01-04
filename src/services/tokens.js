c625c4775ea3c45b54cb6d98089373a1d696da37
Shubham Verma <imshubhamverma.sv@gmail.com>
2021-01-04 00:42:52 +0000

    hardcoded token

--- src/services/tokens.js
+++ src/services/tokens.js
@@ -1,6 +1,6 @@
 const h = new Headers();
 
-h.append("Authorization", `Client-ID ${process.env.REACT_APP_CLIENT_ID}`);
+h.append("Authorization", `Client-ID E5EHMkfEkragu1136UCpc_fGGenGwqZltRj6vgw9Hyk`);
 
 const baseURL = "https://api.unsplash.com";
 
